b5794849cc87fd95c7fcabc8d1e7f47a8f90ff47
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2022-11-25 23:37:29 +0900

    add sketchPicker

--- src/pages/xmas.js
+++ src/pages/xmas.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, useNavigate } from "react-router-dom";
-import tree from '../assets/image/tree.png';
+import tree from '../assets/image/xmastree.png';
+import { SketchPicker } from 'react-color'
+import styled from "styled-components";
+import { ReactImageTint } from 'react-image-tint';
+import './Xmas.css';
+//`
+const St_img=styled.img`
+    //filter: opacity(0.5) drop-shadow(0 0 0, color)
+`;
 function Xmas() {
-  
+   const [color, setColor] = useState("#409A13");
+    
   return (
     <div>
         <p>sdf</p>
-      <img className='xmas-tree' alt="tree" src={tree} />
+      <img className='img-tree' alt="tree" src={tree}
+              tintColor = { color}
+      />
+      <ReactImageTint src={tree} color={color} />;
+      <SketchPicker 
+        color={ color }
+        onChangeComplete={ (color) => {setColor(color.hex)} }/>
     </div>
 
   );
 }
+
 
 export default Xmas;

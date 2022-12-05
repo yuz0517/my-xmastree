6949e603cc4ec706b2a211cdb9933dfaab5bc15d
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2022-12-05 19:32:55 +0900

    add draggable

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,34 +1,52 @@
-import React, { useState } from "react";
-import tree from '../assets/image/xmastree.png';
-import ball1 from '../assets/image/xball1.png';
-import { SketchPicker } from 'react-color'
+import React, { useState, useRef } from "react";
+import tree from "../assets/image/xmastree.png";
+import ball1 from "../img/xball1.png";
+import { SketchPicker } from "react-color";
 import styled from "styled-components";
-import Draggable from 'react-draggable'
-import { ReactImageTint } from 'react-image-tint';
-const St_img = styled.img`
-className='img-tree';
-alt="tree" src={tree};
-tintColor = { color}
-//filter: opacity(0.5) drop-shadow(0 0 0, color)
-`;
+import Draggable from "react-draggable";
+//import { ReactImageTint } from "react-image-tint";
+import './Tree.css'
+
 function Tree() {
-    const [color, setColor] = useState("#409A13");
+  const nodeRef = useRef(null);
+  const [position, setPosition] = useState({ x:0, y:0 });
+  const [Opacity, setOpacity] = useState(false);
+  const handleStart = () => {
+    setOpacity(true);
+  }
+  const handleEnd = () => {
+    setOpacity(false);
+  }
+  const trackBall1Pos = (data) => {
+    setPosition({ x: data.x, y: data.y });
+  }
+  const [color, setColor] = useState("#409A13");
   return (
     <>
-      <img className="img-tree" alt="tree" src={tree} tintColor={color} />
-      <St_img />
-      <ReactImageTint src={tree} color={color} />;
-      <SketchPicker
+      <img className="img-tree" alt="tree" src={tree} tintcolor={color} />
+      <Draggable
+        nodeRef={nodeRef}
+        onDrag={(e, data) => trackBall1Pos(data)}
+        onStart = {handleStart}
+        onStop  = {handleEnd} >
+        <div ref = {nodeRef}
+             className="div-img-ball1"
+             style={{ backgroundImage: `url(${ball1})` }} > </div>
+      </Draggable>
+      <div 
+             className="div-img-ball3"
+             style={{ backgroundImage: `url(${ball1})` }} />
+      
+      
+      {/*<SketchPicker
         color={color}
         onChangeComplete={(color) => {
           setColor(color.hex);
         }}
-      />
-      <Draggable>
-      <img className="img-ball1" alt="ball1" src={ball1} />
+      />*/}
       
-      </Draggable>
+      
     </>
   );
 }
- export default Tree;
+export default Tree;
--- src/pages/xmas.js
+++ src/pages/xmas.js
@@ -3,16 +3,11 @@ import { Navigate, useNavigate } from "react-router-dom";
 import tree from '../assets/image/xmastree.png';
 import { SketchPicker } from 'react-color'
 import styled from "styled-components";
-import { ReactImageTint } from 'react-image-tint';
+//import { ReactImageTint } from 'react-image-tint';
 import './Xmas.css';
 import Tree from '../components/Tree';
 //`
-const St_img=styled.img`
-    className='img-tree';
-    alt="tree" src={tree};
-    tintColor = { color}
-    //filter: opacity(0.5) drop-shadow(0 0 0, color)
-`;
+
 function Xmas() {
   
     

7108e87be7b16814d4b3b1ded8dfd72af221e43b
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2022-12-25 23:56:18 +0900

    add tooltip

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -3,7 +3,7 @@ import html2canvas from "html2canvas";
 
 import tree from "../img/xmastree.png";
 import yellowneon_img from "../img/yellowneon.png";
-import redneon_img from "../img/redneon.png"
+import redneon_img from "../img/redneon.png";
 import ball1 from "../img/xball1.png";
 import { SketchPicker } from "react-color";
 
@@ -12,26 +12,22 @@ import "./Tree.css";
 import Deco from "./Deco";
 import Deco1 from "./Deco1";
 
-import { BiQuestionMark } from "react-icons/bi"
+import { BiQuestionMark } from "react-icons/bi";
 import styled from "styled-components";
-
+import { Tooltip } from "react-tooltip";
+import 'react-tooltip/dist/react-tooltip.css'
 import Ball1_list from "./deco_component/Ball1_list";
 import Ball2_list from "./deco_component/Ball2_list";
 import Ball3_list from "./deco_component/Ball3_list";
 import Candy1_list from "./deco_component/Candy1_list";
 const DivTreeSection = styled.div`
-
-border: 1px solid blue;
-margin-top: 80px;
-margin-bottom: 30px;
-width: 350px;
-height: 600px;
-
-
-
+  border: 1px solid blue;
+  margin-top: 80px;
+  margin-bottom: 30px;
+  width: 350px;
+  height: 600px;
 `;
 const Tree = () => {
-  
   const [color, setColor] = useState("#409A13");
   const onCaptureClick = () => {
     const capture = document.getElementById("treeSection");
@@ -59,11 +55,11 @@ const Tree = () => {
   };
   //---Ball1------------//
   const [List, setList] = useState([]);
-    const nextId = useRef(0);
+  const nextId = useRef(0);
   const getList = (List) => {
-    console.log("Tree.js befroe ,",List)
+    console.log("Tree.js befroe ,", List);
     setList(List);
-    console.log("Tree.js getList()", List)
+    console.log("Tree.js getList()", List);
   };
 
   //---yellowneon------//
@@ -74,45 +70,66 @@ const Tree = () => {
     setYellowneon(yellowneon);
   };
   //----Redneon-----//
-  const[redneon,  setRedneon] = useState(false);
+  const [redneon, setRedneon] = useState(false);
 
   const getRedneon = (Redneon) => {
-    
     setRedneon(Redneon);
   };
   return (
     <div className="div-treepage-full">
       <DivTreeSection id="treeSection">
-         
-        <div className="div-tree" style={{ backgroundImage: `url(${tree})` }}  />
-        <div className="div-yellowneon" style={{backgroundImage: `url(${yellowneon_img})`,display: yellowneon ? "block" : "none"}}/>
-        
-        <div className="div-redneon" style={{backgroundImage: `url(${redneon_img})`,display: redneon ? "block" : "none"}}/>
+        <div className="div-tree" style={{ backgroundImage: `url(${tree})` }} />
+        <div
+          className="div-yellowneon"
+          style={{
+            backgroundImage: `url(${yellowneon_img})`,
+            display: yellowneon ? "block" : "none",
+          }}
+        />
+
+        <div
+          className="div-redneon"
+          style={{
+            backgroundImage: `url(${redneon_img})`,
+            display: redneon ? "block" : "none",
+          }}
+        />
         <Ball1_list />
         <Ball2_list />
-        <Ball3_list/>
-        <Candy1_list/>
+        <Ball3_list />
+        <Candy1_list />
       </DivTreeSection>
-      
+
       <div className="div-decoSection">
-        <Deco  yellowneon={yellowneon} getYellowneon={getYellowneon} 
-               redneon={redneon} getRedneon = {getRedneon} nextId={nextId}
-               List={List} getList={getList}/>
+        <Deco
+          yellowneon={yellowneon}
+          getYellowneon={getYellowneon}
+          redneon={redneon}
+          getRedneon={getRedneon}
+          nextId={nextId}
+          List={List}
+          getList={getList}
+        />
         <Deco1 />
       </div>
-      <BiQuestionMark className="icon-qmark"/>
-      {/*<SketchPicker
+      <div>
+        <BiQuestionMark className="icon-qmark" />
+        {/*<SketchPicker
         color={color}
         onChangeComplete={(color) => {
           setColor(color.hex);
         }}
       />*/}
+        <p id="p-snowman" className="p-snowman"
+           data-tooltip-content="Make your own X-mas Tree!<br />Drag your favorite decorations and place them on the Tree"
+           >☃️</p>
+        <Tooltip anchorId="p-snowman" place="top" type="error"/>
+      </div>
       <div>
-        <button onClick={onCaptureClick}
-                className='btn-download'/>
+        <button onClick={onCaptureClick} className="btn-download" />
       </div>
     </div>
   );
-}
+};
 export default Tree;
-export {DivTreeSection}
+export { DivTreeSection };

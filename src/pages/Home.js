d269cdd409dcba195a07e69d1574d8667732fe92
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2022-12-26 23:56:43 +0900

    fix home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Navigate, useNavigate } from "react-router-dom";
+import Snowfall from 'react-snowfall';
 import './Home.css'
+import snowman from '../assets/image/snowman.png'
+
 function Home() {
   const navigate = useNavigate();
   const onclickbutton = () => {
@@ -8,7 +11,9 @@ function Home() {
   }
   return (
     <div className='div-Home'>
+      <Snowfall snowflakeCount={100}/>
       <button onClick={onclickbutton}>버튼</button>
+      <img className='img-home-snowman' src={snowman} width="500"></img>
     </div>
 
   );
